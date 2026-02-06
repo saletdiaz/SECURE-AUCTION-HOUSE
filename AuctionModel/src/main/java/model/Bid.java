c1e92c995e85bd5c895b28a4dc7351e080452c1b
salet diaz <saalet.diaaz@gmail.com>
2026-02-06 06:22:10 +0100

    Creación de clase Bid

--- AuctionModel/src/main/java/model/Bid.java
+++ AuctionModel/src/main/java/model/Bid.java
@@ -1,4 +1,50 @@
 package model;
 
-public class Bid {
+
+import java.io.Serializable;
+
+/**Representa una oferta de un usuario*/
+public class Bid implements Serializable {
+    private int itemId;
+    private String bidderName;
+    private double amount;
+    private boolean isWinning;
+    public Bid(int itemId, String bidderName, double amount) {
+        this.itemId = itemId;
+        this.bidderName = bidderName;
+        this.amount = amount;
+        this.isWinning = false;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getBidderName() {
+        return bidderName;
+    }
+
+    public void setBidderName(String bidderName) {
+        this.bidderName = bidderName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isWinning() {
+        return isWinning;
+    }
+
+    public void setWinning(boolean winning) {
+        isWinning = winning;
+    }
 }
