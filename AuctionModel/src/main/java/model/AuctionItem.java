2dcddd7a892611094ac63553f2a7ccb68bf1b5bb
salet diaz <saalet.diaaz@gmail.com>
2026-02-06 06:18:58 +0100

    Creación de clase Bid y Auction class terminada

--- AuctionModel/src/main/java/model/AuctionItem.java
+++ AuctionModel/src/main/java/model/AuctionItem.java
@@ -3,6 +3,48 @@ package model;
 import java.io.Serializable;
 
 /**Esta clase representará el producto que se vende.*/
-public class AuctionItem extends Serializable {
+public class AuctionItem implements Serializable {
+    private int id;
+    private String name;
+    private String description;
+    private double startingPrice;
 
+    private AuctionItem(int id, String name, String description, double startingPrice) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startingPrice = startingPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getStartingPrice() {
+        return startingPrice;
+    }
+
+    public void setStartingPrice(double startingPrice) {
+        this.startingPrice = startingPrice;
+    }
 }
